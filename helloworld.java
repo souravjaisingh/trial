b789449ec8ee46417c7dbcd6397639756f850d24
souravjaisingh <41680168+souravjaisingh@users.noreply.github.com>
2018-11-13 19:27:58 +0530

    comment added

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ class helloworld
 {
   public static void main(String args[])
   {
-    System.out.println("Hello world");
+    System.out.println("Hello world"); //program to print helo world
   }
 }
